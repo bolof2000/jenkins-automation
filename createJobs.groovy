63c7952f222bf3c53ff14c90d76531f595c86798
Victor Bolofinde <bolofindeolusegun@yahoo.com>
2021-08-11 19:40:47 -0600

    pipeline

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/bolof2000/spring-boot-api.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
